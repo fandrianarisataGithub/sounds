312edcbc8936aca5908311ee6195cd28118cc423
Fenitriniaina <fandrianarisata2@gmail.com>
2021-04-28 16:59:17 +0300

    start export pdf

---
+++ src/Controller/PDFController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class PDFController extends AbstractController
+{
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    /**
+     * @Route("/pdf_ddj", name ="pdf_ddj")
+     */
+    public function pdf_ddj():Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('pdf/donnee_du_jour_pdf.html.twig');
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("ddj.pdf", [
+            "Attachment" => false
+        ]);
+        return $this->render('pdf/donnee_du_jour_pdf.html.twig');
+    }
+}
