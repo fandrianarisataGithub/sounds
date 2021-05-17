643c15efa25a63f0be7b64ef7a6628d610cbfeef
Fenitriniaina <fandrianarisata2@gmail.com>
2021-05-17 17:02:46 +0300

    finalisation content css fidelisation

--- src/Controller/FidelisationController.php
+++ src/Controller/FidelisationController.php
@@ -30,4 +30,94 @@ class FidelisationController extends AbstractController
             "current_page"      => $data_session['current_page'],
         ]);
     }
+
+     /**
+     * @Route("/profile/fidelisation/cardex/{pseudo_hotel}", name="fidelisation_cardex")
+     */
+    public function fidelisation_cardex(Request $request, SessionInterface $session, $pseudo_hotel): Response
+    {
+        $data_session = $session->get('hotel');
+        $data_session['pseudo_hotel'] = $pseudo_hotel;
+        if($data_session == null){
+            return $this->redirectToRoute("app_logout");
+        }
+        
+        return $this->render('fidelisation/cardex.html.twig', [
+            'fidelisation'  => true,
+            'hotel'         => $pseudo_hotel,
+            "current_page"      => $data_session['current_page'],
+        ]);
+    }
+
+     /**
+     * @Route("/profile/fidelisation/exclusif/{pseudo_hotel}", name="fidelisation_exclusif")
+     */
+    public function fidelisation_exclusif(Request $request, SessionInterface $session, $pseudo_hotel): Response
+    {
+        $data_session = $session->get('hotel');
+        $data_session['pseudo_hotel'] = $pseudo_hotel;
+        if($data_session == null){
+            return $this->redirectToRoute("app_logout");
+        }
+        
+        return $this->render('fidelisation/exclusif.html.twig', [
+            'fidelisation'  => true,
+            'hotel'         => $pseudo_hotel,
+            "current_page"      => $data_session['current_page'],
+        ]);
+    }
+
+     /**
+     * @Route("/profile/fidelisation/preferentiel/{pseudo_hotel}", name="fidelisation_preferentiel")
+     */
+    public function fidelisation_preferentiel(Request $request, SessionInterface $session, $pseudo_hotel): Response
+    {
+        $data_session = $session->get('hotel');
+        $data_session['pseudo_hotel'] = $pseudo_hotel;
+        if($data_session == null){
+            return $this->redirectToRoute("app_logout");
+        }
+        
+        return $this->render('fidelisation/preferentiel.html.twig', [
+            'fidelisation'  => true,
+            'hotel'         => $pseudo_hotel,
+            "current_page"      => $data_session['current_page'],
+        ]);
+    }
+
+     /**
+     * @Route("/profile/fidelisation/privilege/{pseudo_hotel}", name="fidelisation_privilege")
+     */
+    public function fidelisation_privilege(Request $request, SessionInterface $session, $pseudo_hotel): Response
+    {
+        $data_session = $session->get('hotel');
+        $data_session['pseudo_hotel'] = $pseudo_hotel;
+        if($data_session == null){
+            return $this->redirectToRoute("app_logout");
+        }
+        
+        return $this->render('fidelisation/privilege.html.twig', [
+            'fidelisation'  => true,
+            'hotel'         => $pseudo_hotel,
+            "current_page"      => $data_session['current_page'],
+        ]);
+    }
+
+     /**
+     * @Route("/profile/fidelisation/fiche_client/{pseudo_hotel}", name="fidelisation_fiche_client")
+     */
+    public function fidelisation_fiche_clent(Request $request, SessionInterface $session, $pseudo_hotel): Response
+    {
+        $data_session = $session->get('hotel');
+        $data_session['pseudo_hotel'] = $pseudo_hotel;
+        if($data_session == null){
+            return $this->redirectToRoute("app_logout");
+        }
+        
+        return $this->render('fidelisation/fiche_client.html.twig', [
+            'fidelisation'  => true,
+            'hotel'         => $pseudo_hotel,
+            "current_page"      => $data_session['current_page'],
+        ]);
+    }
 }
